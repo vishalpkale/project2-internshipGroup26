e27873a86e582c31f2dd2bc44cc4cde420205164
vishalpkale <102423910+vishalpkale@users.noreply.github.com>
2022-05-04 14:29:50 +0530

    internController

---
+++ src/Controllers/internController.js
@@ -0,0 +1,45 @@
+const interModel = require('../models/internModel')
+const collegeModel=require("../models/collegeModel")
+const mongoose = require('mongoose')
+const EmailValidator=require('email-validator')
+const Validatemobile=require('validate-phone-number-node-js')
+
+const interController = async function (req, res) {
+    try {
+        const { name, email, mobile, collegeId } = req.body;
+        if (!req.body) {
+            res.status(400).send({ status: false, msg: "Please provide valid details" })
+        }
+        if (!name) {
+            res.status(400).send({ status: false, msg: "Please provide valid name" })
+        }
+        if (!email) {
+            res.status(400).send({ status: false, msg: "Please provide valid email" })
+        }
+        if (!mobile) {
+            res.status(400).send({ status: false, msg: "Please provide valid mobile number" })
+        }
+        if (!collegeId) {
+            res.status(400).send({ status: false, msg: "Please provide valid college id" })
+        }
+    if(!EmailValidator.validate(email))
+    {
+        res.status(400).send({ status: false, msg: "Please correct email formate" })
+    }
+    if(!Validatemobile(mobile)){
+        res.status(400).send({ status: false, msg: "Please check mobile number is correct or not" })
+    }
+     const chkcollege=await collegeModel.findById(collegeId)
+     if(!chkcollege)
+     {
+         res.status(400).send({status:false, msg:"college does not exits,please check college id"})
+     }
+        const createIntern = await interModel.create(req.body)
+        res.status(201).send({ status: true, data: createIntern })
+    }
+    catch (err) {
+        res.status(500).send({ status: false, msg: err.msg })
+    }
+}
+
+module.exports.interController = interController;
