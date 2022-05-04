0b6c4f37fa773735613627cacb5f030d2c2d0662
Aditya <adityaverma2004@gmail.com>
2022-05-04 15:23:31 +0530

    newOne

---
+++ src/models/collegeModel.js
@@ -0,0 +1,27 @@
+const { links } = require('express/lib/response')
+const mongoose = require('mongoose')
+
+const collegeSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+        unique: true 
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    logoLink: {
+        type: links,
+        required: true,
+        
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('project2_college', collegeSchema)
